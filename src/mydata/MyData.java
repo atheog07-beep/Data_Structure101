710940fd6c4afd6f8130bb0c6baa2eda396fa802
Ath07 <Ath07@DESKTOP-V5BIE6N>
2026-02-13 16:03:27 +0700

    REPO 1

--- src/mydata/MyData.java
+++ src/mydata/MyData.java
@@ -15,7 +15,14 @@ public class MyData {
      */
     public static void main(String[] args) {
         int[] Yokuwi = {10, 20,30,40,50,60,70,80,90,100};  
+        int jumlah = 0;
+        for (int i = 0; i < Yokuwi.length; i++) {
+            jumlah += Yokuwi[i];
+            
+        }
+        System.out.println("Hasil : " + jumlah);
         
     }
+ 
     
 }
